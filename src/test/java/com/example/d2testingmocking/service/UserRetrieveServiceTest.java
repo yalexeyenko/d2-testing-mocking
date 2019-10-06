414c84b72855037ef8d0d1d9b9cfa4ad6aae88c8
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2019-10-06 21:48:10 +0600

    Break unit test

--- src/test/java/com/example/d2testingmocking/service/UserRetrieveServiceTest.java
+++ src/test/java/com/example/d2testingmocking/service/UserRetrieveServiceTest.java
@@ -49,7 +49,7 @@ public class UserRetrieveServiceTest {
         // when
         UserNotFoundException exception = assertThrows(UserNotFoundException.class, () -> userRetrieveService.findByEmail(email));
         // then
-        assertEquals("User with email " + email + " was not found", exception.getMessage());
+        assertNotEquals("User with email " + email + " was not found", exception.getMessage());
     }
 
     @Test
